441a29a75d8dd7a324da23583ff212f7592c2bc5
Nathan Bartley <bartleyn@uchicago.edu>
2019-11-27 11:15:44 -0800

    cleanup example

--- example.py
+++ example.py
@@ -6,14 +6,13 @@ import numpy as np
 # FILES
 EMBEDDING_FILENAME = './embeddings.emb'
 EMBEDDING_MODEL_FILENAME = './embeddings.model'
-temp_folder = './tmp'
 
 # Create a graph
 graph = nx.fast_gnp_random_graph(n=100, p=0.5)
 
 
 # Precompute probabilities and generate walks
-node2vec = Node2Vec(graph, dimensions=64, walk_length=30, num_walks=200, workers=4, temp_folder=temp_folder)
+node2vec = Node2Vec(graph, dimensions=64, walk_length=30, num_walks=200, workers=4)
 
 ## if d_graph is big enough to fit in the memory, pass temp_folder which has enough disk space
 # Note: It will trigger "sharedmem" in Parallel, which will be slow on smaller graphs
@@ -24,11 +23,6 @@ model = node2vec.fit(window=10, min_count=1, batch_words=4)  # Any keywords acce
 
 # Look for most similar nodes
 print(model.wv.most_similar('2'))  # Output node names are always strings
-
-node2vec = Node2Vec(graph, dimensions=64, walk_length=30, num_walks=200, workers=1, temp_folder=temp_folder)
-model = node2vec.fit(window=10, min_count=1, batch_words=4)  # Any keywords acceptable by gensim.Word2Vec can be passed, `diemnsions` and `workers` are automatically passed (from the Node2Vec constructor)
-print(model.wv.most_similar('2'))  # Output node names are always strings
-
 
 
 # Save embeddings for later use
